64d6adca8c3506ca8ad5f1202973c492511cc9c7
angk500 <nikita@schaffnern.ch>
2025-03-08 14:33:30 +0100

    Added versioning

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup, find_packages
+from srgssr_news_downloader import __version__
 
 with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='SRG SSR News Downloader',
-    version='1!1.0.0',
-    description='Simple Download Tool for the SRG SSR News as audio file.',
-    author='Nikita Schaffner',
-    author_email='dev@schaffnern.ch',
-    url='https://github.com/nikitaschaffner/srgssr_news_downloader',
-    license=license,
-    packages=find_packages()
+    name            =   'SRG SSR News Downloader',
+    version         =   __version__,
+    description     =   'Simple Download Tool for the SRG SSR News as audio file.',
+    author          =   'Nikita Schaffner',
+    author_email    =   'dev@schaffnern.ch',
+    url             =   'https://github.com/nikitaschaffner/srgssr_news_downloader',
+    license         =   license,
+    packages        =   find_packages()
 )
